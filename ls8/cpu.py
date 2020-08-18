5b485a0cf64b0fb440df5add5b37ca612c5bf5a5
Issac Moreno <issaxavier909@gmail.com>
2020-08-18 16:39:24 -0700

    Day 2 - MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -25,24 +25,46 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
+        
+        if len(sys.argv) != 2:
+            print("usage: comp.py filename")
+            sys.exit()
+        
+        try:
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    comment_split = line.split('#')[0]
+                    cmd = comment_split.strip()
 
-        # For now, we've just hardcoded a program:
+                    if cmd == "":
+                        continue
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    instruction = int(cmd, 2)
+                    self.ram[address] = instruction
+                    address += 1
+        
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} file was not found")
+            sys.exit()
+            
+        # address = 0
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # # For now, we've just hardcoded a program:
+
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
